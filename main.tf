dd1a6dccb181f21d16f6db230ae4894d08fa8652
JunTaeINC <juntea3523@naver.com>
2023-06-27 11:13:35 +0900

    Create aws_subnet

--- main.tf
+++ main.tf
@@ -31,3 +31,12 @@ resource "aws_vpc" "example_vpc" {
     Name = "example-vpc"
   }
 }
+
+resource "aws_subnet" "example_subnet" {
+  vpc_id     = aws_vpc.example_vpc.id
+  cidr_block = "10.0.1.0/24"
+
+  tags = {
+    Name = "example-subnet"
+  }
+}
